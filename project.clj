2dd161cab251d2b23066c119164029e1d7bce7e8
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-03-16 14:31:20 -0400

    v0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.leanpixel/om-material "0.2.0"
+(defproject org.clojars.leanpixel/om-material "0.2.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
 
